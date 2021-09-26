ef2be20c5d02fa68c2629bb928dc400f7d31362f
Duncan <1658485+ddycai@users.noreply.github.com>
2021-09-26 10:50:14 -0700

    Add firebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,80 @@ import FilterNav from './components/FilterNav';
 import { useState } from 'react';
 import { Hidden, Drawer, SwipeableDrawer } from '@material-ui/core';
 
+import {firebaseConfig} from './firebaseSetup';
+import {Auth, getAuth, getRedirectResult, signInWithRedirect, onAuthStateChanged} from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+
+  const toggleSignIn = (auth: Auth) => {
+    if (!auth.currentUser) {
+      const provider = new GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/plus.login');
+      signInWithRedirect(auth, provider);
+    } else {
+      auth.signOut();
+    }
+  }
+
 const App = () => {
   // const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
   const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
   const [mobileOpen, setMobileOpen] = useState(false);
-  
+  const [signInState, setSignInState] = useState(false);
+ 
   const handleFilterToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+
+  let token;
+  let user;
+
+  getRedirectResult(auth)
+  .then((result) => {
+    if (result) {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      token = credential?.accessToken;
+
+      // The signed-in user info.
+      user = result.user;
+    }
+  }).catch((error) => {
+
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+  onAuthStateChanged(auth, function(user) {
+    if (user) {
+      // User is signed in.
+      // var displayName = user.displayName;
+      // var email = user.email;
+      // var emailVerified = user.emailVerified;
+      // var photoURL = user.photoURL;
+      // var isAnonymous = user.isAnonymous;
+      // var uid = user.uid;
+      // var providerData = user.providerData;
+      console.log('user', user);
+      setSignInState(true);
+    } else {
+      // User is signed out.
+      setSignInState(false);
+    }
+  });
+
   return (
     <div className="App">
+      <button id='sign-in' onClick={() => toggleSignIn(auth)}>{signInState ? 'Sign Out' : 'Sign In'}</button>
       <h1>Weekly Reflections</h1>
       <button className="filterBtn" onClick={handleFilterToggle}>Filters</button>
 
---
+++ src/firebaseSetup.tsx
@@ -0,0 +1,14 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBlMr8a-dSYAt5MfsxEm98NBMRz43gjhe8",
+  authDomain: "weekly-reflections.firebaseapp.com",
+  projectId: "weekly-reflections",
+  storageBucket: "weekly-reflections.appspot.com",
+  messagingSenderId: "895305424138",
+  appId: "1:895305424138:web:308d7f52ad1c4033716ef3",
+  measurementId: "G-Q4EX8F804J"
+};
